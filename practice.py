0458e17e1cc915a96909f1affd6ec81a4553be29
hyojin17 <chj7960@likelion.org>
2019-03-10 14:51:05 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,8 @@
+lion_we_know=['hyojin', 'seol', 'enjin','kijung']
+lion_meet = ['hyojin', 'enjin', 'bak']
+
+for lion in lion_meet:
+    if lion in lion_we_know:
+        print("hello!")
+    else:
+        print("I don't know")
